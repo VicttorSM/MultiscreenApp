ed8e0cd832d2551bc7819c35dd45ad02ed15f661
Victtor <victtordasilva@gmail.com>
2019-03-30 06:44:21 -0300

    Troca de tela funcionando

--- app/src/main/java/com/example/android/multiscreenapp/MainActivity.java
+++ app/src/main/java/com/example/android/multiscreenapp/MainActivity.java
@@ -1,9 +1,9 @@
 package com.example.android.multiscreenapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,8 +25,13 @@ public class MainActivity extends AppCompatActivity {
         btCadastroCliente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                openCadastroClienteActivity();
             }
         });
+    }
+
+    public void openCadastroClienteActivity() {
+        Intent intent = new Intent(this, CadastroClienteActivity.class);
+        startActivity(intent);
     }
 }
