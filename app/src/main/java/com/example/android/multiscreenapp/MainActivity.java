a9726b76924842a89d9fb5ead455923f00ca258f
Victtor <victtordasilva@gmail.com>
2019-03-30 00:19:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/android/multiscreenapp/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.android.multiscreenapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton btCadastroCliente;
+    ImageButton btCadastroProduto;
+    ImageButton btListagem;
+    ImageButton btConfiguracoes;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btCadastroCliente = findViewById(R.id.btCadastroCliente);
+        btCadastroProduto = findViewById(R.id.btCadastroProduto);
+        btListagem = findViewById(R.id.btListagem);
+        btConfiguracoes = findViewById(R.id.btConfiguracoes);
+
+        btCadastroCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
